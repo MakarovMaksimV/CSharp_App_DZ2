d61080c6e6c1d4cefd73163e929fbadb847f7b20
Maksim <mirror0791@mail.ru>
2024-09-10 22:56:53 +0300

    Inc

---
+++ DZ2/IBitGateble.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DZ2
+{
+	public interface IBitGateble
+	{
+        bool GetBitByIndex(byte index);
+        void SetBitByIndex(byte index, bool value);
+    }
+}
+
---
+++ DZ2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DZ2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bits byteBits = new Bits(4);
+        Bits intBits = new Bits(257);
+        Bits longBits = new Bits(3_000_000_000);
+        
+        byte byteVal = (byte)byteBits;
+        Console.WriteLine(byteVal);
+
+        int intVal = (int)intBits;
+        Console.WriteLine(intVal);
+
+        long longVal = (long)longBits;
+        Console.WriteLine(longVal);
+
+        Console.ReadKey();
+    }
+}
+
